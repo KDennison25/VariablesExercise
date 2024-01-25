f67ab7cfadd9c45dc419cfc9a7a0cbc0217585ba
Student <student@BCCSs-MacBook-Pro.local>
2024-01-25 10:56:40 -0600

    Exercise and Bonus completed

--- script.js
+++ script.js
@@ -12,4 +12,4 @@ blank = null // #9
 const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01 // #1
 console.log(quarter,dime,nickel,penny); // #2
 let letters = 'qwertyuiop'  // #3
-console.log(letters[9]);
+console.log(letters[letters.length-1]); // #4
