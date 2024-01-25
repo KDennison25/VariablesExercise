67a4e019664f7275e2916324f7bc48b6ddcc36f3
Student <student@BCCSs-MacBook-Pro.local>
2024-01-25 10:04:17 -0600

    Exercise completed

---
+++ script.js
@@ -0,0 +1,15 @@
+// Const #1
+let num = 20  // #2
+num = 15 // #3
+num+= 7 // #4
+num-- // #5
+num = 'This is not a number' // #6
+let blank // #7
+// console.log(num);
+console.log(blank); // #8
+blank = null // #9
+
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01 // #1
+console.log(quarter,dime,nickel,penny); // #2
+let letters = 'qwertyuiop'  // #3
+console.log(letters[9]);
